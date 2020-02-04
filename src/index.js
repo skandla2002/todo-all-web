88505c87a25dd38786fabd747eaa455be6c708d0
skandla2002 <skandla2002@gmail.com>
2020-02-05 00:39:49 +0900

    yarn eject 후

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import rootReducer from "./store/modules";
 import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import registerServiceWorker from "./registerServiceWorker";
+
+// 리덕스 개발자 도구 적용
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
