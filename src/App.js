890bc9ccf1cbe54956c856207a38ded3a5d5e4cf
JONGMEONG PAK <skandla2002@gmail.com>
2020-02-10 00:01:38 +0900

    라우팅 처리

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import React, {Component} from "react";
 // import List from './components/List';
 // import Login from "./components/Login";
 import CounterContainer from "./container/CounterContainer";
+import Login from './components/Login';
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 // import Counter from './components/Counter';
 import "./App.css";
 
 class App extends Component {
   render(){
       return (
-      <div className="App">
-        <CounterContainer />
-      </div>
+      <Router>
+        <div className="App">
+            <Route exact path="/" component={Login}></Route>
+            <Route exact path="/counter" component={CounterContainer}></Route>
+            {/* <Link to="/">{CounterContainer}</Link>
+            <Link to="/Login">{Login}</Link>         */}
+        </div>
+      </Router>
     );
   }
 }
