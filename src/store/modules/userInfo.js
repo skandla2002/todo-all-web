50a1a8d75bbb7dd582808edcb852ea55aba7bd67
JONGMEONG PAK <skandla2002@gmail.com>
2020-02-11 06:32:00 +0900

    유저 정보 획득 reducer 생성

---
+++ src/store/modules/userInfo.js
@@ -0,0 +1,40 @@
+const GETUSERINFO = 'getInfo/USER';
+const SETUSERINFO = 'setInfo/USER';
+
+export const getUserInfo = () => {
+    return {
+        type: GETUSERINFO
+    }    
+}
+export const setUserInfo = () => {
+    return { 
+        type: SETUSERINFO
+    }
+} 
+
+const initialData = {
+    name: '',
+    age: '',
+    birth: '',
+    sex: '',
+    email: '',
+    id: '',
+    nickName: ''
+}
+
+export function userInfo (state = initialData, action ) {
+    switch(action.type){
+        case GETUSERINFO:
+            return {
+                ...state,
+                ...action.data
+            }
+        case SETUSERINFO:
+            return {
+                ...state,
+                ...action.data
+            }
+        default:
+            return state;
+    }
+}
