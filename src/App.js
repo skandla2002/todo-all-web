7f8994bd78b816e69d95475167ec29825add322c
JONGMEONG PAK <skandla2002@gmail.com>
2020-02-16 23:48:52 +0900

    switch 처리

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, {Component} from "react";
 // import Login from "./components/Login";
 import CounterContainer from "./container/CounterContainer";
 import Login from './components/Login';
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Profile from './components/Profile';
@@ -24,15 +24,25 @@ class App extends Component {
               </li>
             </ul>
               
-            {/* <Route exact path="/" component={Login}></Route> */}
-            <Route path={["/About", "/info"]} component={About}></Route>
-            {/* <Route path="/profile/:username" component={Profile}></Route> */}
-            <Route path="/Home" component={Home}></Route>
-            <Route exact path="/counter" component={CounterContainer}></Route>
-            <Route path="/profiles" component={Profiles}></Route>
-            <Route path="/history" component={HistorySample}></Route>
-            {/* <Link to="/">{CounterContainer}</Link>
-            <Link to="/Login">{Login}</Link>         */}
+            <Switch>
+              {/* <Route exact path="/" component={Login}></Route> */}
+              <Route path={["/About", "/info"]} component={About}></Route>
+              {/* <Route path="/profile/:username" component={Profile}></Route> */}
+              <Route path="/Home" component={Home}></Route>
+              <Route exact path="/counter" component={CounterContainer}></Route>
+              <Route path="/profiles" component={Profiles}></Route>
+              <Route path="/history" component={HistorySample}></Route>
+              {/* <Link to="/">{CounterContainer}</Link>
+              <Link to="/Login">{Login}</Link>         */}
+              <Route 
+                render={({location}) => (
+                  <div>
+                    <h2>이 페이지는 존재하지 않습니다:</h2>
+                    <p>{location.pathname}</p>
+                  </div>
+                )}
+              />
+            </Switch>
         </div>
       </Router>
     );
