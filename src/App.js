d59b2983c09c556e6ca38003b0b808dc0756aed3
JONGMEONG PAK <skandla2002@gmail.com>
2020-02-16 22:28:25 +0900

    리엑트 라우트 설정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, {Component} from "react";
 import CounterContainer from "./container/CounterContainer";
 import Login from './components/Login';
 import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
 // import Counter from './components/Counter';
 import "./App.css";
 
@@ -13,8 +15,10 @@ class App extends Component {
       return (
       <Router>
         <div className="App">
-            <Route exact path="/" component={Login}></Route>
-            <Route exact path="/counter" component={CounterContainer}></Route>
+            <Route exact="true" path="/" component={Login}></Route>
+            <Route path={["/About", "/info"]} component={About}></Route>
+            <Route path="/Home" component={Home}></Route>
+            <Route exact="true" path="/counter" component={CounterContainer}></Route>
             {/* <Link to="/">{CounterContainer}</Link>
             <Link to="/Login">{Login}</Link>         */}
         </div>
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const About = () => {
+    return (
+        <div>
+            <div>About...</div>
+            <Link to="/home">Home으로</Link>
+        </div>
+    )
+};
+
+export default About;
